436915e258df2bd1337f6a2714cdfdfe39d18260
Tim Cuthbertson <tim@gfxmonk.net>
2019-10-26 11:35:15 +1100

    stop wrapping exportLocal paths in a derivation, it causes unpack issues

--- nix/exportLocalGit.nix
+++ nix/exportLocalGit.nix
@@ -34,15 +34,8 @@
 		commitDrv = builtins.fetchGit { url = path; rev = commit; };
 		refDrv = builtins.fetchGit { url = path; rev = (resolveRef ref); inherit ref; };
 		workspaceDrv = builtins.fetchGit { url = path; };
-		ensureRealDerivation = base: if lib.isDerivation base then base else stdenv.mkDerivation {
-			name = "src";
-			passthru = { path = base; };
-			buildCommand = ''
-				ln -sfn ${base} $out
-			'';
-		};
 	in
-	ensureRealDerivation (lib.findFirst (x: x.condition == true) { drv = commitRequired; } [
+	(lib.findFirst (x: x.condition == true) { drv = commitRequired; } [
 		{ condition = commit != null && ref == null && !workingChanges; drv = commitDrv; }
 		{ condition = commit == null && ref != null && !workingChanges; drv = refDrv; }
 		{ condition = commit == null && ref == null && workingChanges;  drv = workspaceDrv; }
